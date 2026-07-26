9182124ccc3e703c153f43d5452b55d47b4c2d41
Duncan Campbell <campbellduncan12@gmail.com>
2026-07-25 19:17:59 -0500

    Refactor boilerplate

--- src/main/java/com/mrfuzzihead/fluiddrawers/FluidDrawers.java
+++ src/main/java/com/mrfuzzihead/fluiddrawers/FluidDrawers.java
@@ -11,13 +11,16 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
 @Mod(
-    modid = com.mrfuzzihead.fluiddrawers.FluidDrawers.MODID,
+    modid = FluidDrawers.MODID,
     version = Tags.VERSION,
-    name = "FluidDrawers",
+    name = FluidDrawers.MODNAME,
+    dependencies = FluidDrawers.DEPENDENCIES,
     acceptedMinecraftVersions = "[1.7.10]")
 public class FluidDrawers {
 
     public static final String MODID = "fluiddrawers";
+    public static final String MODNAME = "Fluid Drawers";
+    public static final String DEPENDENCIES = "after:StorageDrawers;";
     public static final Logger LOG = LogManager.getLogger(MODID);
 
     @SidedProxy(
@@ -26,26 +29,21 @@ public class FluidDrawers {
     public static CommonProxy proxy;
 
     @Mod.EventHandler
-    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
-    // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
         proxy.preInit(event);
     }
 
     @Mod.EventHandler
-    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
     public void init(FMLInitializationEvent event) {
         proxy.init(event);
     }
 
     @Mod.EventHandler
-    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
     }
 
     @Mod.EventHandler
-    // register server commands in this event handler (Remove if not needed)
     public void serverStarting(FMLServerStartingEvent event) {
         proxy.serverStarting(event);
     }
